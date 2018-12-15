7012a2cf83ebbc1e4bc73257c69afebe2a99be6a
monsieurluge <monsieurluge@gmail.com>
2018-12-15 22:23:56 +0100

    create "combined values" interface and object

---
+++ src/Result/BaseCombinedValues.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace monsieurluge\Result\Result;
+
+/**
+ * A simple implementation of the CombinedValues interface.
+ */
+final class BaseCombinedValues implements CombinedValues
+{
+
+    /** @var mixed **/
+    private $first;
+    /** @var mixed **/
+    private $second;
+
+    /**
+     * @codeCoverageIgnore
+     *
+     * @param mixed $first
+     * @param mixed $second
+     */
+    public function __construct($first, $second)
+    {
+        $this->first  = $first;
+        $this->second = $second;
+    }
+
+    /**
+    * @inheritDoc
+    */
+    public function first()
+    {
+        return $this->first;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function second()
+    {
+        return $this->second;
+    }
+
+}
