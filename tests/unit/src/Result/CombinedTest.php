a8f4abec1cc455d53accbf2252197a8788cca07f
monsieurluge <monsieurluge@gmail.com>
2018-12-15 22:30:59 +0100

    new tests: check the combined result's return when there is a failure

--- tests/unit/src/Result/CombinedTest.php
+++ tests/unit/src/Result/CombinedTest.php
@@ -3,14 +3,17 @@
 namespace tests\unit\Result;
 
 use Closure;
+use monsieurluge\Result\Error\BaseError;
 use monsieurluge\Result\Error\Error;
 use monsieurluge\Result\Result\Combined;
+use monsieurluge\Result\Result\Failure;
 use monsieurluge\Result\Result\Result;
 use monsieurluge\Result\Result\Success;
 use PHPUnit\Framework\TestCase;
 
 final class CombinedTest extends TestCase
 {
+
     /**
      * @covers Combined::getValueOrExecOnFailure
      */
@@ -29,6 +32,68 @@ final class CombinedTest extends TestCase
         $this->assertSame('test', $testSubject->first());
 
         $this->assertSame('ok', $testSubject->second());
+    }
+
+    /**
+     * @covers Combined::getValueOrExecOnFailure
+     */
+    public function testGetValueOfSuccessAndFailureReturnsTheError()
+    {
+        // GIVEN
+        $combined = new Combined(
+            new Success('test'),
+            new Failure(
+                new BaseError('err-1234', 'failure')
+            )
+        );
+
+        // WHEN
+        $testSubject = $combined->getValueOrExecOnFailure($this->returnErrorMessage());
+
+        // THEN
+        $this->assertSame('failure', $testSubject);
+    }
+
+    /**
+     * @covers Combined::getValueOrExecOnFailure
+     */
+    public function testGetValueOfFailureAndSuccessReturnsTheError()
+    {
+        // GIVEN
+        $combined = new Combined(
+            new Failure(
+                new BaseError('err-1234', 'failure')
+            ),
+            new Success('test')
+        );
+
+        // WHEN
+        $testSubject = $combined->getValueOrExecOnFailure($this->returnErrorMessage());
+
+        // THEN
+        $this->assertSame('failure', $testSubject);
+    }
+
+    /**
+     * @covers Combined::getValueOrExecOnFailure
+     */
+    public function testGetValueOfFailuresReturnsTheFirstError()
+    {
+        // GIVEN
+        $combined = new Combined(
+            new Failure(
+                new BaseError('err-1234', 'failure')
+            ),
+            new Failure(
+                new BaseError('err-4567', 'error')
+            )
+        );
+
+        // WHEN
+        $testSubject = $combined->getValueOrExecOnFailure($this->returnErrorMessage());
+
+        // THEN
+        $this->assertSame('failure', $testSubject);
     }
 
     /**
