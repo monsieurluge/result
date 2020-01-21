336c849efb82ae61bc82578a9c711c5b4f089561
monsieurluge <monsieurluge@gmail.com>
2020-01-21 09:59:07 +0100

    test update: check for the right exception

--- tests/unit/Error/FileErrorFactoryTest.php
+++ tests/unit/Error/FileErrorFactoryTest.php
@@ -2,12 +2,25 @@
 
 namespace tests\unit\Error;
 
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
+use monsieurluge\Result\ErrorFactory\FileErrorFactory;
 
 final class FileErrorFactoryTest extends TestCase
 {
+    /**
+     * @covers monsieurluge\Result\ErrorFactory\FileErrorFactory::__construct
+     */
     public function testExceptionIsThrownWhenConfigFileDoesntExist()
     {
-        // todo
+        // GIVEN a wrong config file path
+        $file = '/foo/bar/foo/bar';
+        // AND the expected exception
+        $this->expectException(InvalidArgumentException::class);
+
+        // WHEN the error factory object is created
+        new FileErrorFactory($file);
+
+        // THEN the expected exception is thrown
     }
 }
