29dfbc9eb8eeab099c2b95f63bb54d4086e15f71
monsieurluge <monsieurluge@gmail.com>
2020-01-21 09:46:38 +0100

    create the FileErrorFactory tests suite squeleton

---
+++ tests/unit/Error/FileErrorFactoryTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tests\unit\Error;
+
+use PHPUnit\Framework\TestCase;
+
+final class FileErrorFactoryTest extends TestCase
+{
+    public function testExceptionIsThrownWhenConfigFileDoesntExist()
+    {
+        // todo
+    }
+}
